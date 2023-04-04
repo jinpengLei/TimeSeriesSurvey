e975b791aa5597f0980cad2edc269c71051faa66
jinpeng <1308629473@qq.com>
2023-04-04 22:17:58 +0800

    设置实验参数

---
+++ runQI.sh
@@ -0,0 +1,13 @@
+export CUDA_VISIBLE_DEVICES=1
+
+python -u run.py --data EXC --root_path ./dataset/Custom --data_path exchange_rate.txt --model Lstm --binary 1 --mpa 2
+
+python -u run.py --data EXC --root_path ./dataset/Custom --data_path exchange_rate.txt --model Lstm --binary 2 --mpa 2
+
+python -u run.py --data EXC --root_path ./dataset/Custom --data_path exchange_rate.txt --model TpaLstm --binary 1 --mpa 2
+
+python -u run.py --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv  --model Lstm --binary 1 --mpa 2
+
+python -u run.py --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model Lstm --binary 2 --mpa 2
+
+python -u run.py --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model TpaLstm --binary 1 --mpa 2
