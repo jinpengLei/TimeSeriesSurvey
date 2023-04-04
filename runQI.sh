ae6d55454f6c0397150db2d7ca05d43bf241eec7
jinpeng <1308629473@qq.com>
2023-04-04 22:24:33 +0800

    设置实验参数

--- runQI.sh
+++ runQI.sh
@@ -1,13 +1,13 @@
 export CUDA_VISIBLE_DEVICES=1
 
-python -u run.py --data EXC --root_path ./dataset/Customer --data_path exchange_rate.txt --model Lstm --binary 1 --mpa 2
+python -u run.py --data EXC --root_path ./dataset/Customer --data_path exchange_rate.txt --model Lstm --binary 1 --mpa 2 --train_epoch 30
 
-python -u run.py --data EXC --root_path ./dataset/Customer --data_path exchange_rate.txt --model Lstm --binary 2 --mpa 2
+python -u run.py --data EXC --root_path ./dataset/Customer --data_path exchange_rate.txt --model Lstm --binary 2 --mpa 2 --train_epochs 30
 
-python -u run.py --data EXC --root_path ./dataset/Customer --data_path exchange_rate.txt --model TpaLstm --binary 1 --mpa 2
+python -u run.py --data EXC --root_path ./dataset/Customer --data_path exchange_rate.txt --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
 
-python -u run.py --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv  --model Lstm --binary 1 --mpa 2
+python -u run.py --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv  --model Lstm --binary 1 --mpa 2 --train_epochs 30
 
-python -u run.py --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model Lstm --binary 2 --mpa 2
+python -u run.py --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model Lstm --binary 2 --mpa 2  --train_epochs 30
 
-python -u run.py --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model TpaLstm --binary 1 --mpa 2
+python -u run.py --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
