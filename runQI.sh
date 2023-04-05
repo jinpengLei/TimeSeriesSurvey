38283a4a4bb80ab3e7bf31fd7063ab7a0eecdca9
jinpeng <1308629473@qq.com>
2023-04-05 08:07:26 +0800

    更新运行命令

--- runQI.sh
+++ runQI.sh
@@ -11,3 +11,21 @@ python -u run.py --label_len 0 --pred_len 1  --data ETTm1 --root_path ./dataset/
 python -u run.py --label_len 0 --pred_len 1  --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model Lstm --binary 2 --mpa 2  --train_epochs 30
 
 python -u run.py --label_len 0 --pred_len 1  --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTm2 --root_path ./dataset/ETT --data_path ETTm2.csv  --model Lstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTm2 --root_path ./dataset/ETT --data_path ETTm2.csv --model Lstm --binary 2 --mpa 2  --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTm2 --root_path ./dataset/ETT --data_path ETTm2.csv --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh1 --root_path ./dataset/ETT --data_path ETTh1.csv  --model Lstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh1 --root_path ./dataset/ETT --data_path ETTh1.csv --model Lstm --binary 2 --mpa 2  --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh1 --root_path ./dataset/ETT --data_path ETTh1.csv --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh2 --root_path ./dataset/ETT --data_path ETTh2.csv  --model Lstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh2 --root_path ./dataset/ETT --data_path ETTh2.csv --model Lstm --binary 2 --mpa 2  --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh2 --root_path ./dataset/ETT --data_path ETTh2.csv --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
