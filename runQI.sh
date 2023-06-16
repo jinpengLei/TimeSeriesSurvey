ac87482f2266114be88e9ac6ff9032b7c34417eb
jinpeng <1308629473@qq.com>
2023-06-16 15:30:36 +0800

    毕业前更新

---
+++ runQI.sh
@@ -0,0 +1,38 @@
+export CUDA_VISIBLE_DEVICES=1
+if [ -d "best_result" ]; then
+    rm -rf best_result
+fi
+
+if [ -d "best_model" ]; then
+    rm -rf best_model
+fi
+
+python -u run.py --label_len 0 --pred_len 1  --data EXC --root_path ./dataset/Customer --data_path exchange_rate.txt --model Lstm --binary 1 --mpa 2 --train_epoch 30
+
+python -u run.py --label_len 0 --pred_len 1  --data EXC --root_path ./dataset/Customer --data_path exchange_rate.txt --model Lstm --binary 2 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data EXC --root_path ./dataset/Customer --data_path exchange_rate.txt --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv  --model Lstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model Lstm --binary 2 --mpa 2  --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTm1 --root_path ./dataset/ETT --data_path ETTm1.csv --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTm2 --root_path ./dataset/ETT --data_path ETTm2.csv  --model Lstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTm2 --root_path ./dataset/ETT --data_path ETTm2.csv --model Lstm --binary 2 --mpa 2  --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTm2 --root_path ./dataset/ETT --data_path ETTm2.csv --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh1 --root_path ./dataset/ETT --data_path ETTh1.csv  --model Lstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh1 --root_path ./dataset/ETT --data_path ETTh1.csv --model Lstm --binary 2 --mpa 2  --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh1 --root_path ./dataset/ETT --data_path ETTh1.csv --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh2 --root_path ./dataset/ETT --data_path ETTh2.csv  --model Lstm --binary 1 --mpa 2 --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh2 --root_path ./dataset/ETT --data_path ETTh2.csv --model Lstm --binary 2 --mpa 2  --train_epochs 30
+
+python -u run.py --label_len 0 --pred_len 1  --data ETTh2 --root_path ./dataset/ETT --data_path ETTh2.csv --model TpaLstm --binary 1 --mpa 2 --train_epochs 30
